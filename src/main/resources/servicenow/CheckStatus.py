7e04235902c03f044b8910b3322a78da742f1049
Rick Broker <rbroker@xebialabs.com>
2016-04-02 08:26:28 -0400

    Refactor, remove included http class

--- src/main/resources/servicenow/CheckStatus.py
+++ src/main/resources/servicenow/CheckStatus.py
@@ -6,31 +6,25 @@
 
 import sys, string, time
 import com.xhaus.jyson.JysonCodec as json
-
-RECORD_CHECK_STATUS = 200
+from servicenow.ServiceNowClient import ServiceNowClient
 
 if servicenowServer is None:
     print "No server provided."
     sys.exit(1)
 
-servicenowUrl = servicenowServer['url']
-
-credentials = CredentialsFallback(servicenowServer, username, password).getCredentials()
+snClient = ServiceNowClient.create_client(servicenowServer, username, password)
 content = None
-
 
 print "Sending content %s" % content
 
-servicenowAPIUrl = servicenowUrl + '/api/now/v1/table/' + tableName + '/' + sysId
-
-servicenowResponse = XLRequest(servicenowAPIUrl, 'GET', content, credentials['username'], credentials['password'], 'application/json').send()
-
-if servicenowResponse.status == RECORD_CHECK_STATUS:
-    data = json.loads(servicenowResponse.read())
-    status = data["result"]["state"]
+try:
+    data = snClient.get_change_request(tableName, sysId)
+    status = data[statusField]
+    ticket = data['number']
     print "Found %s in Service Now." % (sysId)
-else:
-    print "Failed to check record in Service Now"
-    servicenowResponse.errorDump()
+    print json.dumps(data, indent=4, sort_keys=True)
+except:
+    print json.dumps(data, indent=4, sort_keys=True)
+    print "Error finding status for %s" % statusField
     sys.exit(1)
-
+# End try
--- src/main/resources/servicenow/PollingCheckStatus.py
+++ src/main/resources/servicenow/PollingCheckStatus.py
@@ -6,44 +6,31 @@
 
 import sys, string, time
 import com.xhaus.jyson.JysonCodec as json
-
-RECORD_CHECK_STATUS = 200
+from servicenow.ServiceNowClient import ServiceNowClient
 
 if servicenowServer is None:
     print "No server provided."
     sys.exit(1)
 
-servicenowUrl = servicenowServer['url']
-
-credentials = CredentialsFallback(servicenowServer, username, password).getCredentials()
-content = None
-
-
-print "Sending content %s" % content
-
-servicenowAPIUrl = servicenowUrl + '/api/now/v1/table/' + tableName + '/' + sysId
-
 isClear = False
+snClient = ServiceNowClient.create_client(servicenowServer, username, password)
 
 while ( not isClear ):
 
   try:
-    servicenowResponse = XLRequest(servicenowAPIUrl, 'GET', content, credentials['username'], credentials['password'], 'application/json').send()
+    data = snClient.get_change_request(tableName, sysId)
 
-    if servicenowResponse.status == RECORD_CHECK_STATUS:
-        data = json.loads(servicenowResponse.read())
-        status = data["result"][statusField]
-        if status == checkForStatus :
-             print "Found %s in Service Now." % (sysId)
-             print data
-             isClear = True
-        # End if
-    else:
-        print "Failed to check record in Service Now"
-        servicenowResponse.errorDump()
-        #sys.exit(1)
+    status = data[statusField]
+    if status == checkForStatus :
+         print "Found %s in Service Now." % (sysId)
+         status = data[statusField]
+         ticket = data["number"]
+         print "Found %s in Service Now." % (sysId)
+         print json.dumps(data, indent=4, sort_keys=True)
+         isClear = True
     # End if
   except:
+        print json.dumps(data, indent=4, sort_keys=True)
         print "Error finding status for %s" % statusField
   # End try
 
