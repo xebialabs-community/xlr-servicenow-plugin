b44f259c29ceeb23f74bb255281a1f63eb3b09c2
Joris De Winne <joris.dewinne@gmail.com>
2017-05-25 15:07:40 -0700

    Updating gradle plugins

--- src/test/resources/docker/initialize/initialize_data.sh
+++ src/test/resources/docker/initialize/initialize_data.sh
@@ -23,12 +23,5 @@ BASEDIR=$(dirname $0)
         -d @$BASEDIR/data/ServiceNow_Example.json\
     http://localhost:5516/api/v1/templates/import
 
-#
-#    curl -u admin:admin \
-#        -H "Accept: application/json" \
-#        -H "Content-type: application/json" \
-#        -X POST \
-#        -d @$BASEDIR/data/Express_Scripts_Master.json \
-#    http://localhost:5516/api/v1/templates/import
 
 exit 0
--- src/test/resources/docker/initialize/install_templates.sh
+++ src/test/resources/docker/initialize/install_templates.sh
@@ -18,9 +18,3 @@ curl -u admin:admin \
     -d @$BASEDIR/data/ServiceNow_Example.json\
 http://localhost:5516/api/v1/templates/import
 
-#curl -u admin:admin \
-#    -H "Accept: application/json" \
-#    -H "Content-type: application/json" \
-#    -X POST \
-#    -d @$BASEDIR/data/Express_Scripts_Master.json \
-#http://localhost:5516/api/v1/templates/import
