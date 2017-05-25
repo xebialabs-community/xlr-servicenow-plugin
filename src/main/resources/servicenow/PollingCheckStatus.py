0430e849d8159300b700ce367113e564b2a62804
Joris De Winne <joris.dewinne@gmail.com>
2017-05-25 15:33:47 -0700

    Removing unused import

--- src/main/resources/servicenow/PollingCheckStatus.py
+++ src/main/resources/servicenow/PollingCheckStatus.py
@@ -8,8 +8,9 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-import sys, string, time, traceback
-import com.xhaus.jyson.JysonCodec as json
+import sys
+import time
+import traceback
 from servicenow.ServiceNowClient import ServiceNowClient
 
 if servicenowServer is None:
@@ -28,35 +29,32 @@ if pollInterval is None:
     print "No pollInterval provided."
     sys.exit(1)
 
-
 isClear = False
 snClient = ServiceNowClient.create_client(servicenowServer, username, password)
 data = ""
 
-while ( not isClear ):
+while (not isClear):
 
-  try:
-    data = snClient.get_change_request(tableName, sysId)
+    try:
+        data = snClient.get_change_request(tableName, sysId)
 
-    status = data[statusField]
-    if status == checkForStatus :
-         status = data[statusField]
-         ticket = data["number"]
-         print "Found %s in Service Now." % (sysId)
-         isClear = True
-    # End if
-  except Exception, e:
-    exc_info = sys.exc_info()
-    traceback.print_exception( *exc_info )
-    print e
-    print snClient.print_error( e )
-    print "Error finding status for %s" % statusField
-  # End try
+        status = data[statusField]
+        if status == checkForStatus:
+            status = data[statusField]
+            ticket = data["number"]
+            print "Found %s in Service Now." % (sysId)
+            isClear = True
+            # End if
+    except Exception, e:
+        exc_info = sys.exc_info()
+        traceback.print_exception(*exc_info)
+        print e
+        print snClient.print_error(e)
+        print "Error finding status for %s" % statusField
+    # End try
 
-  time.sleep( pollInterval )
+    time.sleep(pollInterval)
 # End While
 
 print "\n"
-print snClient.print_record( data )
-
-
+print snClient.print_record(data)
