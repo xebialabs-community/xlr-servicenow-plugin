2ee2a1f6bfc8e4e4a282eff3dd748be616cafbcd
Joris De Winne <joris.dewinne@gmail.com>
2018-06-12 17:09:27 -0700

    Added test connection tests for shared configuration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Improvement
+- Updated nebula release plugin to v6.3.5
+- Updated xl-test-api to v0.0.4
+- Added test connection tests for shared configuration
+
 ## v4.1.14
 
 ### Fixed
--- src/test/java/com.xebialabs.xlr.xlrelease.test/LoginTest.java
+++ src/test/java/com.xebialabs.xlr.xlrelease.test/LoginTest.java
@@ -29,6 +29,48 @@ public class LoginTest extends BaseTest {
         SharedConfigurationPropertiesPage.checkSharedConfigurationHeader("ServiceNow");
     }
 
+    @Test
+    public void SaveConfiguration(){
+        MainMenu.clickMenu("Settings");
+        SubMenu.clickSubMenu("Shared configuration");
+        SharedConfigurationPage.openSharedConfiguration("ServiceNow: Server");
+        SharedConfigurationPropertiesPage.checkSharedConfigurationHeader("ServiceNow");
+        SharedConfigurationPropertiesPage.setEditFieldBySequence(1,"ServiceNow Config 1");
+        SharedConfigurationPropertiesPage.setEditFieldBySequence(2,"ServiceNow url");
+        SharedConfigurationPropertiesPage.clickButtonByText("Save");
+        SharedConfigurationPropertiesPage.isNewConfigurationSaved().isSharedConfigurationPageVisible("ServiceNow: Server");
+    }
+
+    @Test
+    public void TestInvalidConfiguration(){
+        MainMenu.clickMenu("Settings");
+        SubMenu.clickSubMenu("Shared configuration");
+        SharedConfigurationPage.openSharedConfiguration("ServiceNow: Server");
+        SharedConfigurationPropertiesPage.checkSharedConfigurationHeader("ServiceNow");
+        SharedConfigurationPropertiesPage.setEditFieldBySequence(1,"ServiceNow Config 2");
+        SharedConfigurationPropertiesPage.setEditFieldBySequence(2,"ServiceNow Url");
+        SharedConfigurationPropertiesPage.clickElementById("authenticationMethod"); // clicking the element so that select field will be visible on next step
+        SharedConfigurationPropertiesPage.setOptionFromSelectFieldBySequence(1,"None");
+        SharedConfigurationPropertiesPage.clickButtonByText("Test");
+        SharedConfigurationPropertiesPage.checkConnectionStatusShouldContain("Can't connect");
+    }
+
+    @Test
+    public void TestValidConfiguration(){
+        MainMenu.clickMenu("Settings");
+        SubMenu.clickSubMenu("Shared configuration");
+        SharedConfigurationPage.openSharedConfiguration("ServiceNow: Server");
+        SharedConfigurationPropertiesPage.checkSharedConfigurationHeader("ServiceNow");
+        SharedConfigurationPropertiesPage.setEditFieldBySequence(1,"ServiceNow Config 3");
+        SharedConfigurationPropertiesPage.setEditFieldBySequence(2, System.getenv("SERVICENOW_URL"));
+        SharedConfigurationPropertiesPage.clickElementById("authenticationMethod"); // clicking the element so that select field will be visible on next step
+        SharedConfigurationPropertiesPage.setOptionFromSelectFieldBySequence(1,"Basic");
+        SharedConfigurationPropertiesPage.setEditFieldBySequence(7, System.getenv("SERVICENOW_USERNAME"));
+        SharedConfigurationPropertiesPage.setEditFieldBySequence(8, System.getenv("SERVICENOW_PASSWORD"));
+        SharedConfigurationPropertiesPage.clickButtonByText("Test");
+        SharedConfigurationPropertiesPage.checkConnectionStatusShouldContain("ServiceNow: Server is available");
+    }
+
     @AfterMethod
     public void logout(){
         System.out.println("called after method");
