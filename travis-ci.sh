1a1897019ae6fb2615a75ada682d13efa4a385d9
ndebuhr <ndebuhr@protonmail.com>
2019-05-15 08:29:22 -0400

    Refine and refactor travis CI script and itest target docker setup, using xlr-rally-plugin as precedent.  Major changes include using docker-compose with the credential updater and the use of a dedicated travis CI script file

---
+++ travis-ci.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p ~/xl-licenses
+http --check-status --print=b --auth $username:$password --json POST https://download.xebialabs.com/api/temporary/xl-release firstName=xlc lastName=xlc email=xlc@xebialabs.com company=xebialabs | jq --raw-output '.license' > ~/xl-licenses/xl-release-license.lic
+./gradlew clean assemble
+export plugin_jar=$(ls build/libs | sort -n | head -1)
+docker-compose -f src/test/resources/docker/docker-compose.yml up -d
+docker wait credentials_updater
+docker logs xlr
+./gradlew itest -PCHROME_HEADLESS_MODE=true
+docker-compose -f src/test/resources/docker/docker-compose.yml stop
+docker-compose -f src/test/resources/docker/docker-compose.yml rm -f
